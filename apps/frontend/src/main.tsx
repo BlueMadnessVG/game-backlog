17f0c22b76718ac52488eaa21fa7e11009423a91
Christian Armenta <car_arca@hotmail.com>
2026-04-08 19:33:56 -0700

    refactor: change router config to use tanstack router index

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react';
 
+import { RouterProvider } from '@tanstack/react-router';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
 import { AppProvider } from './components/provider/provider.tsx';
-import { RouterProvider } from './router/router.tsx';
+import { router } from './router/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProvider>
-      <RouterProvider />
+      <RouterProvider router={router} />
     </AppProvider>
   </StrictMode>,
 );
