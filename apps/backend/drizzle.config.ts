0b27c077a0a13dfae0d33ac967db1d825cd0d9e9
Christian Armenta <car_arca@hotmail.com>
2026-04-13 19:20:08 -0700

    feat: backend drizzle configuration

---
+++ apps/backend/drizzle.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "drizzle-kit";
+
+const DATABASE_URL = Bun.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error(
+    "❌ DATABASE_URL is not defined in your environment variables.",
+  );
+}
+
+export default defineConfig({
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: DATABASE_URL,
+  },
+  breakpoints: true,
+  verbose: true,
+  strict: true,
+});
