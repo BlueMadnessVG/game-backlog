af6c7f53eef1e75b9c847a5a51219f2189a1c612
Christian Armenta <car_arca@hotmail.com>
2026-04-15 18:22:59 -0700

    feat: library manager init

---
+++ apps/frontend/src/features/Library/LibraryManager.tsx
@@ -0,0 +1,19 @@
+import { useLibrary } from './hooks/useLibrary.context';
+
+function LibraryManager() {
+  const { games, isLoading /* , refetch  */ } = useLibrary();
+
+  if (isLoading) return <> isLoading </>;
+
+  if (isLoading && !games) {
+    return <div className="p-8 text-center text-zinc-500">Scanning sector for games...</div>;
+  }
+
+  if (!games || games.length === 0) {
+    return <> no games </>;
+  }
+
+  return <div>nothing for the moment</div>;
+}
+
+export default LibraryManager;
